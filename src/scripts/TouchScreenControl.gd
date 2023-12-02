7575fd25ccdbc7c90f7bc6fb2863a1ecd31b2da2
Alexey Korepanov <alexkorep@gmail.com>
2023-12-02 17:56:21 +0100

    Update touch screen movement algorithm

--- src/scripts/TouchScreenControl.gd
+++ src/scripts/TouchScreenControl.gd
@@ -2,7 +2,7 @@ extends Control
 
 var initial_touch_position = null
 var swipe_threshold = 50
-var moving = false
+var cur_action = null
 
 func release_actions():
 	Input.action_release("move_left")
@@ -11,23 +11,24 @@ func release_actions():
 	Input.action_release("move_down")
 
 func _input(event):
-	#print(event)
 	if (event is InputEventScreenTouch or event is InputEventMouseButton) and event.pressed:
 		initial_touch_position = event.position
 	elif event is InputEventScreenDrag or event is InputEventMouseMotion and initial_touch_position != null:
 		var drag_position = event.position
+		var action = null
 		if drag_position.x < initial_touch_position.x - swipe_threshold: # Threshold for swipe detection
-			Input.action_press("move_left")
-			initial_touch_position = null # Reset to avoid multiple detections
+			action = "move_left"
 		elif drag_position.x > initial_touch_position.x + swipe_threshold: # Threshold for swipe detection
-			Input.action_press("move_right")
-			initial_touch_position = null # Reset to avoid multiple detections
+			action = "move_right"
 		elif drag_position.y < initial_touch_position.y - swipe_threshold: # Threshold for swipe detection
-			Input.action_press("move_up")
-			initial_touch_position = null # Reset to avoid multiple detections
+			action = "move_up"
 		elif drag_position.y > initial_touch_position.y + swipe_threshold: # Threshold for swipe detection
-			Input.action_press("move_down")
-			initial_touch_position = null # Reset to avoid multiple detections
+			action = "move_down"
+		if action and action != cur_action:
+			release_actions()
+			Input.action_press(action)
+			cur_action = action
 	elif (event is InputEventScreenTouch or event is InputEventMouseButton) and not event.pressed:
 		release_actions()
+		cur_action = null
 		initial_touch_position = null
