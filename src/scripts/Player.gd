13ebfa58053a027fd474c7e6b4bffdb47d3695fb
Alexey Korepanov <alexkorep@gmail.com>
2023-12-02 20:19:06 +0100

    Add cloud sprite direction

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -5,8 +5,24 @@ const MOTION_SPEED = 160 # Pixels/second.
 
 func _physics_process(_delta):
 	var motion = Vector2()
-	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	var left = Input.get_action_strength("move_left")
+	var right = Input.get_action_strength("move_right")
+	var down = Input.get_action_strength("move_down")
+	var up = Input.get_action_strength("move_up")
+	var frame = -1
+	if up:
+		frame = 2
+	elif left:
+		frame = 0
+	elif right:
+		frame = 1
+	elif down:
+		frame = 3
+	if frame >= 0:
+		$Sprite.frame = frame
+		
+	motion.x = right - left
+	motion.y = down - up
 	motion.y /= 2
 	motion = motion.normalized() * MOTION_SPEED
 	#warning-ignore:return_value_discarded
