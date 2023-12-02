9c12ef2102b609254af1962e5ffe68dbf6036ae8
Alexey Korepanov <alexkorep@gmail.com>
2023-12-02 21:02:16 +0100

    Updates

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1701547123|5762571719";
+const CACHE_VERSION = "1701547328|5967495537";
 const CACHE_PREFIX = "Candy Maze-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
