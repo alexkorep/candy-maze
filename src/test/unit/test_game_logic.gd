74c3b1ace98eaf20002aa7c2ef08f4e17b40e348
Alexey Korepanov <alexkorep@gmail.com>
2023-12-06 20:35:54 +0100

    Fix the tests

--- src/test/unit/test_game_logic.gd
+++ src/test/unit/test_game_logic.gd
@@ -2,10 +2,10 @@ extends "res://addons/gut/test.gd"
 
 var game_logic
 
-func setup():
+func before_each():
 	game_logic = load("res://scripts/GameLogic.gd").new()
 
-func teardown():
+func after_each():
 	game_logic.free()
 
 func test_can_move():
@@ -23,6 +23,34 @@ func test_can_move():
 
 	# Test moving into an empty space
 	assert_true(game_logic.can_move(Vector2.DOWN))
+
+func test_move_into_box_can_be_moved():
+	# Setup
+	game_logic.level_state = {
+		'walls': [Vector2(3, 0)],
+		'player': { 'current': Vector2.ZERO, 'previous': Vector2.ZERO, 'intermediate': Vector2.ZERO },
+		'boxes': [{ 'current': Vector2(1, 0), 'previous': Vector2.ZERO, 'intermediate': Vector2.ZERO }],
+		'targets': []
+	}
+	game_logic.move_timer = game_logic.move_duration
+
+	# Test moving into a box that can be moved
+	assert_true(game_logic.can_move(Vector2.RIGHT))
+
+
+func test_move_into_box_cannot_be_moved():
+	# Reset the level state
+	game_logic.level_state = {
+		'walls': [Vector2(2, 0)],
+		'player': { 'current': Vector2.ZERO, 'previous': Vector2.ZERO, 'intermediate': Vector2.ZERO },
+		'boxes': [{ 'current': Vector2(1, 0), 'previous': Vector2.ZERO, 'intermediate': Vector2.ZERO }],
+		'targets': []
+	}
+
+	game_logic.move_timer = game_logic.move_duration
+
+	# Test moving into a box that cannot be moved
+	assert_false(game_logic.can_move(Vector2.RIGHT))
 
 func test_move():
 	# Setup
@@ -49,3 +77,33 @@ func test_is_level_complete():
 
 	# Test level complete
 	assert_true(game_logic.is_level_complete())
+
+func test_move_into_box_can_move():
+	# Setup
+	game_logic.level_state = {
+		'walls': [Vector2(3, 0)],
+		'player': { 'current': Vector2.ZERO, 'previous': Vector2.ZERO, 'intermediate': Vector2.ZERO },
+		'boxes': [{ 'current': Vector2(1, 0), 'previous': Vector2.ZERO, 'intermediate': Vector2.ZERO }],
+		'targets': []
+	}
+	game_logic.move_timer = game_logic.move_duration
+
+	# Test moving into a box that can be moved
+	game_logic.move(Vector2.RIGHT)
+	assert_eq(game_logic.level_state['player']['current'], Vector2.RIGHT)
+	assert_eq(game_logic.level_state['boxes'][0]['current'], Vector2(2, 0))
+
+func test_move_into_box_cannot_move():
+	# Reset the level state
+	game_logic.level_state = {
+		'walls': [Vector2(1, 0)],
+		'player': { 'current': Vector2.ZERO, 'previous': Vector2.ZERO, 'intermediate': Vector2.ZERO },
+		'boxes': [{ 'current': Vector2(1, 0), 'previous': Vector2.ZERO, 'intermediate': Vector2.ZERO }],
+		'targets': []
+	}
+	game_logic.move_timer = game_logic.move_duration
+
+	# Test moving into a box that cannot be moved
+	game_logic.move(Vector2.RIGHT)
+	assert_eq(game_logic.level_state['player']['current'], Vector2.ZERO)
+	assert_eq(game_logic.level_state['boxes'][0]['current'], Vector2(1, 0))
