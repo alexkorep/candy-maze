8d0e52ec741edff6addf4c065119668453296339
Alexey Korepanov <alexkorep@gmail.com>
2023-12-09 18:44:12 +0100

    Add a new background

--- src/scripts/Main.gd
+++ src/scripts/Main.gd
@@ -3,9 +3,9 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var game_field = $GameField
-	game_field.rect_scale = Vector2(0, 0)
+	game_field.rect_scale = Vector2(0.5, 0.5)
 	var tween_scale = create_tween().set_trans(Tween.TRANS_SINE)
-	tween_scale.tween_property(game_field, "rect_scale", Vector2(1, 1), 2)
+	tween_scale.tween_property(game_field, "rect_scale", Vector2(1, 1), 0.5)
 	tween_scale.set_loops(1)
 
 
